51a697d4dfe5b11ff401815920582c01a1aa930b
Madhu <visitmadhureddy@gmail.com>
2022-01-03 08:30:57 +0000

    Pipeline Testing First Commit

---
+++ scripted.tf
@@ -0,0 +1,12 @@
+node {
+    stage("Build") {
+        echo "Some code compilation here..."
+    }
+    stage("deploy") {
+        echo "Some code compilation here..."
+    }
+    stage("Test") {
+        echo "Some tests execution here..."
+        echo "1"
+    }
+}
